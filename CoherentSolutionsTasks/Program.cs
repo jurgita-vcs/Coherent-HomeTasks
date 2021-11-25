46fc3ee8932a46ebabb9c7fea73d4eb0ee50a32a
Jurgita Damušienė <jurgitabal@gmail.com>
2021-11-25 15:11:44 +0200

    Add project files.

---
+++ CoherentSolutionsTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoherentSolutionsTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
