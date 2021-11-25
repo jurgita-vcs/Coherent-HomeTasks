3c507d528aca24af272b1140025d8398a18bf976
jurgita-vcs <jurgitabal@gmail.com>
2021-11-25 16:52:44 +0200

    Arrays implementation.

--- CoherentSolutionsTasks/Program.cs
+++ CoherentSolutionsTasks/Program.cs
@@ -4,9 +4,27 @@ namespace CoherentSolutionsTasks
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please, enter the number of elements in a numeric array (at least two)");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            PrintArray(array);
+        }
+
+
+        private static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Array[{i}]= {arr[i]}");
+            }
         }
     }
 }
