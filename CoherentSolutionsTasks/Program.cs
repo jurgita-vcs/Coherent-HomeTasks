dfc2fe6cc0326eccd45299140d3bc38582faa9c1
jurgita-vcs <jurgitabal@gmail.com>
2021-11-29 15:19:29 +0200

    Find min and max

--- CoherentSolutionsTasks/Program.cs
+++ CoherentSolutionsTasks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace CoherentSolutionsTasks
 {
@@ -7,24 +8,73 @@ namespace CoherentSolutionsTasks
         public static void Main(string[] args)
         {
             Console.WriteLine("Please, enter the number of elements in a numeric array (at least two)");
-            int n = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
 
-            int[] array = new int[n];
+            var array = new int[n];
 
-            for (int i = 0; i < n; i++)
+            for (var i = 0; i < n; i++)
             {
                 array[i] = int.Parse(Console.ReadLine());
             }
-            PrintArray(array);
-        }
 
+            Console.Write("Given array: ");
+            PrintArray(array);
+            Console.WriteLine();
+            
+            Console.WriteLine($"Sum between {MinSearch(array)} and {MaxSearch(array)} = " + 
+                              $"{SumBetweenMinMax(array)}");
+        }
 
         private static void PrintArray(int[] arr)
         {
-            for (int i = 0; i < arr.Length; i++)
+            for (var i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine($"Array[{i}]= {arr[i]}");
+                Console.Write(arr[i] + " ");
             }
+        }
+
+        private static int MaxSearch(int[] arr)
+        {
+            var max = arr[0];
+
+            foreach (var i in arr)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+
+            return max;
+        }
+
+        private static int MinSearch(int[] arr)
+        {
+            var min = arr[0];
+
+            foreach (var i in arr)
+            {
+                if (i < min)
+                {
+                    min = i;
+                }
+            }
+
+            return min;
+        }
+
+        private static int SumBetweenMinMax(int[] arr)
+        {
+            var sum = 0;
+            var min = arr.Min();
+            var max = arr.Max();
+
+            for (var i = min; i <= max; i++)
+            {
+                sum = sum + arr[i];
+            }
+
+            return sum;
         }
     }
 }
