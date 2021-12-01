ea0532883c7d59e69625344710fbbe92a8f4f865
jurgita-vcs <jurgitabal@gmail.com>
2021-12-01 13:09:09 +0200

    Add new project Task1-01

---
+++ Task01-1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Task01_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter two integer numbers");
+            try
+            {
+                var a = int.Parse(Console.ReadLine());
+                var b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ternary numbers sequence: ");
+                PrintTernaryNumber(a, b);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please, input integer numbers");
+            }
+        }
+        private static bool IsNumberTernary(int number)
+        {
+            while (number > 0)
+            {
+                var num = number % 10;
+
+                if ((num != 0) && (num != 1) && (num != 2))
+                {
+                    return (false);
+                }
+                number = number / 10;
+
+                if (number == 0)
+                {
+                    return (true);
+                }
+            }
+            return false;
+        }
+        private static void PrintTernaryNumber(int a, int b)
+        {
+            if (a < b)
+            {
+                for (var i = a; i <= b; i++)
+                {
+                    if (IsNumberTernary(i))
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+            else
+            {
+                for (var i = a; i >= b; i--)
+                {
+                    if (IsNumberTernary(i))
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+        }
+    }
+}
